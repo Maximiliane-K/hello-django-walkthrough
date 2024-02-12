dfc52c081e0144428edbf81e4e2489679c26ccad
Maximiliane <maximiliane.kaempffer@gmail.com>
2024-02-12 19:00:10 +0000

    Add toggle button, view and path

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -19,9 +19,9 @@ from todo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', get_todo_list, name='get_todo_list'),
+    path('', views.get_todo_list, name='get_todo_list'),
     path('add', views.add_item, name='add'),
     path('edit/<item_id>', views.edit_item, name='edit'),
-    ath('toggle/<item_id>', views.toggle_item, name='toggle')
+    path('toggle/<item_id>', views.toggle_item, name='toggle'),
 ]
 
